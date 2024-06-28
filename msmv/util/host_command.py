7408a99f8a0ad4641676e0586b94e3425dac22dc
Easton Elliott <easton@geekness.eu>
2024-06-28 02:31:00 -0700

    Add binary mode and next command option to run another command piping in

--- msmv/util/host_command.py
+++ msmv/util/host_command.py
@@ -15,39 +15,58 @@ class HostCommand:
         stdin=subprocess.PIPE,
         env=None,
         verbose=False,
+        binary_mode=False,
+        next_command=None,
     ):
         print(f"Running command: {' '.join(command)} in dir {cwd}")
         try:
-            with subprocess.Popen(
+            # Setup the first process
+            process = subprocess.Popen(
                 command,
-                stdout=stdout,
+                stdout=subprocess.PIPE if next_command else stdout,
                 stderr=stderr,
                 stdin=stdin,
-                text=True,
                 cwd=cwd,
                 shell=shell,
                 env=env,
-            ) as process:
-                try:
-                    if verbose:
-                        for line in process.stdout:
-                            print(line, end="")
-                    else:
-                        # Wait for the process to complete and get stderr
-                        stdout, stderr = process.communicate(timeout=timeout)
-                        if stdout:
-                            print(stdout)
-                        if stderr:
-                            print(stderr)
-                    if process.returncode != 0:
-                        print("Error executing command:", stderr)
-                        exit(1)
-                    return stdout, stderr
-                except subprocess.TimeoutExpired:
-                    process.kill()
-                    _, stderr = process.communicate()
-                    print(f"Process timed out with error: {stderr}")
-                    exit(1)
+                text=not binary_mode,
+            )
+
+            # If there's a next command, set it up to receive input from the first process
+            if next_command:
+                next_process = subprocess.Popen(
+                    next_command,
+                    stdin=process.stdout,
+                    stdout=stdout,
+                    stderr=stderr,
+                    cwd=cwd,
+                    shell=shell,
+                    env=env,
+                    text=not binary_mode,
+                )
+                # Allow process to receive a SIGPIPE if next_process exits
+                process.stdout.close()
+                output, error = next_process.communicate(timeout=timeout)
+                exit_code = next_process.returncode
+            else:
+                output, error = process.communicate(timeout=timeout)
+                exit_code = process.returncode
+
+            if verbose and output:
+                print(output.decode() if not binary_mode else output)
+
+            if exit_code != 0:
+                error_msg = error.decode() if not binary_mode else error
+                print(f"Error executing command: {error_msg}")
+                exit(1)
+
+            return output, error
+        except subprocess.TimeoutExpired as e:
+            process.kill()
+            if next_command:
+                next_process.kill()
+            print(f"Process timed out with error: {e}")
+            exit(1)
         except subprocess.CalledProcessError as e:
             print(f"An error occurred: {e}")
             exit(1)
